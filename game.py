db88990df13861023048a36fcadcab0dff3eb96d
mandeepsidhu2 <mandeep.sidhu2@gmail.com>
2023-03-25 18:11:48 -0400

    not null

--- game.py
+++ game.py
@@ -11,6 +11,11 @@ cardArr = ['A','2','3','4','5','6','7','8','9','10','J','Q','K',
 
 
 class BlackJackGame:
+	def getCard(self):
+		card = self.shoe[self.currShoeIdx]
+		self.currShoeIdx+=1
+		return card
+
 	def startRound(self):
 		self.playerVsHands = {}
 		for x in range(self.numPlayers+1): 
@@ -19,10 +24,8 @@ class BlackJackGame:
 			cards = []
 			if x == 0: # this loop is to assign card to the dealer
 				self.currDealerHiddenCard = self.currShoeIdx
-			cards.append(self.shoe[self.currShoeIdx])
-			self.currShoeIdx+=1
-			cards.append(self.shoe[self.currShoeIdx])
-			self.currShoeIdx+=1
+			cards.append(self.getCard())
+			cards.append(self.getCard())
 			self.playerVsHands[x].extend(cards)
 	def getSumOfCards(self,cards):
 		sum1=0
@@ -30,7 +33,7 @@ class BlackJackGame:
 		for card in cards:
 			if card == 'A':
 				sum1+=1
-				sum1+=11
+				sum2+=11
 			elif card == 'J' or card == 'Q' or card == 'K':
 				sum1+=10
 				sum2+=10
@@ -44,8 +47,11 @@ class BlackJackGame:
 		self.numDecks = math.ceil(numGameRounds * 5 * float(numPlayers+1) / numGameRounds)
 		self.shoe = []
 		self.currShoeIdx = 0
-		self.playIdx = 0 
+		self.playIdx = 1
 		self.currDealerHiddenCard = None
+		self.currentGameRound = 1
+		self.numGameRounds = numGameRounds
+		self.gamePlayingHistory = {}
 		# 0 means player 1, when playIdx is numPlayers it means the dealerr
 		# because he plays the last
 		self.playerVsHands = None
@@ -56,13 +62,19 @@ class BlackJackGame:
 
 	def getNumberOfCardsLeftInShoe(self):
 		return len(self.shoe)-self.currShoeIdx+1
+
+
 	def getDealerTopCard(self):
 		dealerHand = self.playerVsHands[0]
 		# dealer will have only 1 hand (he cannot split)
 		cardArr =  dealerHand[0]
 		return cardArr[-1]
+
+
 	def getCurrentTurn(self):
 		return playIdx
+
+
 	def getLegalMoves(self):
 		# 0 idx is the dealer, 1 .. numPlayers are the players
 		nextPlayIdx = self.playIdx
@@ -77,11 +89,99 @@ class BlackJackGame:
 		else: #  currently  one of the players is playing 
 			return ['Hit','Stand']
 
+	def getOptimalSum(self,cardSumTuple):
+		cardOptimalSum = 0
+		if cardSumTuple[0]>cardSumTuple[1] :
+			if cardSumTuple[0]<=21:
+				cardOptimalSum = cardSumTuple[0]
+			else:
+				cardOptimalSum = cardSumTuple[1]
+		elif cardSumTuple[0]<=cardSumTuple[1] :
+			if cardSumTuple[1]<=21:
+				cardOptimalSum = cardSumTuple[1]
+			else:
+				cardOptimalSum = cardSumTuple[0]
+		return cardOptimalSum
+
+	def findGameWinners(self):
+
+		dealerCardSum = self.getSumOfCards(self.playerVsHands[0])
+		print("**",dealerCardSum)
+		dealerCardSum = self.getOptimalSum(dealerCardSum)
+		ans = []
+		for i in range(1,self.numPlayers+1):
+			playerCardSum = self.getSumOfCards(self.playerVsHands[i])
+			playerCardSum = self.getOptimalSum(playerCardSum)
+			if playerCardSum > dealerCardSum:
+				if playerCardSum <= 21:
+					ans.append('Win')
+				else:
+					ans.append('Lost')
+
+			elif playerCardSum < dealerCardSum:
+				if dealerCardSum <= 21:
+					ans.append('Lost')
+				else:
+					ans.append('Win')
+			else:
+				ans.append('Tied')
+			print("Player ",i,ans[i-1], playerCardSum,"vs",dealerCardSum)
+		return ans
+	def getGamePlayingHistory(self):
+		return self.gamePlayingHistory
+	def makeMove(self,move):
+		print("The current player playing is ",self.playIdx)
+		if self.playIdx == 0:
+			agentCards = self.playerVsHands[0]
+			agentCardsSum = self.getSumOfCards(agentCards)
+			while agentCardsSum[0]<17 and agentCardsSum[1]<17:
+				card = self.getCard()
+				self.playerVsHands[0].append(card)
+				agentCardsSum = self.getSumOfCards(self.playerVsHands[0])
+			self.gamePlayingHistory[self.currentGameRound]=self.findGameWinners()
+			print(game.playerVsHands,'\n')
+			self.startRound()
+			self.currentGameRound+=1
+			if self.currentGameRound > self.numGameRounds:
+				print("Game ends !")
+
+
+		# if the player is moving 'Stand', do nothing and if he hits just add the card,
+		# whether he loses or wins is implicit after the dealer makes the move
+		elif move=='Hit':
+			self.playerVsHands[self.playIdx].append(self.getCard())
+
+
+
+
+		# after the current player has played, increment hthe player idx
+		self.playIdx=self.playIdx+1
+		if self.playIdx == (self.numPlayers+1):
+			self.playIdx = 0
+
+
+
 
 game = BlackJackGame(4,2)
 print(game.playerVsHands)
 print(game.getNumberOfCardsLeftInShoe())
 print(game.getDealerTopCard())
 print(game.getLegalMoves())
+
+game.makeMove('Hit')
+print(game.playerVsHands,'\n')
+game.makeMove('Hit')
+print(game.playerVsHands,'\n')
+game.makeMove('Hit')
+print(game.playerVsHands,'\n')
+game.makeMove('Hit')
+print(game.playerVsHands,'\n')
+game.makeMove('Hit')
+print(game.playerVsHands,'\n')
+game.makeMove('Hit')
+
+#print(game.playerVsHands,'\n')
+
+print(game.getGamePlayingHistory())
 
 		
