52c807a9ffff269b1e9ea3d0f53379656bd8df08
mandeepsidhu2 <mandeep.sidhu2@gmail.com>
2023-03-24 21:47:27 -0400

    not null

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ cardArr = ['A','2','3','4','5','6','7','8','9','10','J','Q','K',
 'A','2','3','4','5','6','7','8','9','10','J','Q','K',
 'A','2','3','4','5','6','7','8','9','10','J','Q','K',
 'A','2','3','4','5','6','7','8','9','10','J','Q','K']
-#moves = Enum('Begin','Hitting','Standing')
+#moves = Enum('Begin','Hit','Stand')
 # Initialise the game object with the number of games you want to play
 #class Hand(object):
 
@@ -24,6 +24,20 @@ class BlackJackGame:
 			cards.append(self.shoe[self.currShoeIdx])
 			self.currShoeIdx+=1
 			self.playerVsHands[x].extend(cards)
+	def getSumOfCards(self,cards):
+		sum1=0
+		sum2=0
+		for card in cards:
+			if card == 'A':
+				sum1+=1
+				sum1+=11
+			elif card == 'J' or card == 'Q' or card == 'K':
+				sum1+=10
+				sum2+=10
+			else:
+				sum1+=int(card)
+				sum2+=int(card)
+		return (sum1,sum2)
 
 	def __init__(self, numPlayers, numGameRounds):
 		self.numPlayers = numPlayers
@@ -49,15 +63,25 @@ class BlackJackGame:
 		return cardArr[-1]
 	def getCurrentTurn(self):
 		return playIdx
-	#def getLegalMoves(self):
-		# if playIdx == numPlayers: # curringently the leader is play
-		# 	hands = playerVsHands[playIdx]
-		# else: #  currently  one of the players is playing 
+	def getLegalMoves(self):
+		# 0 idx is the dealer, 1 .. numPlayers are the players
+		nextPlayIdx = self.playIdx
+		if nextPlayIdx == (self.numPlayers+1): # curringently the leader is play
+			nextPlayIdx = 0
+		cards = self.playerVsHands[nextPlayIdx]
+		cardSumTuple = self.getSumOfCards(cards)
+		if cardSumTuple[0]<17 and cardSumTuple[1]<17:
+			return ['Hit']
+		elif cardSumTuple[0]>=17 and cardSumTuple[1]>=17:
+			return ['Stand']
+		else: #  currently  one of the players is playing 
+			return ['Hit','Stand']
+
 
 game = BlackJackGame(4,2)
 print(game.playerVsHands)
 print(game.getNumberOfCardsLeftInShoe())
 print(game.getDealerTopCard())
-
+print(game.getLegalMoves())
 
 		
